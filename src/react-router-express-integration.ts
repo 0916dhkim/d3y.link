75856bf37ee51a1238463953250bd4ed60f68750
Danny Kim <0916dhkim@gmail.com>
2025-05-14 03:04:41 -0400

    Make the integration code more readable

--- src/react-router-express-integration.ts
+++ src/react-router-express-integration.ts
@@ -1,41 +1,57 @@
 import { createRequestHandler } from "@react-router/express";
 import { ServerBuild } from "react-router";
 import express, { type Express } from "express";
+import { type ViteDevServer } from "vite";
 
-const COMPILED_SERVER_PATH = "../build/server/index.js";
+export async function integrateReactRouter(app: Express) {
+  const isProduction = process.env.NODE_ENV === "production";
 
+  if (isProduction) {
+    registerCompiledAssetMiddlewares(app);
+    await registerProdSsrMiddleware(app);
+  } else {
+    const viteDevServer = await createViteDevServer();
+    app.use(viteDevServer.middlewares);
+    await registerDevSsrMiddleware(app, viteDevServer);
+  }
+}
+
+function registerCompiledAssetMiddlewares(app: Express) {
+  app.use(
+    "/assets",
+    express.static("build/client/assets", { immutable: true, maxAge: "1y" })
+  );
+  app.use(express.static("build/client"));
+}
+
+async function registerProdSsrMiddleware(app: Express) {
+  const COMPILED_SSR_HANDLER_PATH = "../build/server/index.js";
+  app.use(
+    createRequestHandler({ build: await import(COMPILED_SSR_HANDLER_PATH) })
+  );
+}
+
+async function registerDevSsrMiddleware(
+  app: Express,
+  viteDevServer: ViteDevServer
+) {
+  app.use(
+    createRequestHandler({
+      build: () =>
+        viteDevServer.ssrLoadModule(
+          "virtual:react-router/server-build"
+        ) as Promise<ServerBuild>,
+    })
+  );
+}
+
+/**
+ * Create a Vite dev server to serve the bundled client assets.
+ */
 async function createViteDevServer() {
   const vite = await import("vite");
   const server = await vite.createServer({
     server: { middlewareMode: true },
   });
   return server;
-}
-
-export async function integrateReactRouter(app: Express) {
-  const isProduction = process.env.NODE_ENV === "production";
-
-  if (isProduction) {
-    // PROD
-    app.use(
-      "/assets",
-      express.static("build/client/assets", { immutable: true, maxAge: "1y" })
-    );
-    app.use(express.static("build/client"));
-    app.use(
-      createRequestHandler({ build: await import(COMPILED_SERVER_PATH) })
-    );
-  } else {
-    // DEV
-    const viteDevServer = await createViteDevServer();
-    app.use(viteDevServer.middlewares);
-    app.use(
-      createRequestHandler({
-        build: () =>
-          viteDevServer.ssrLoadModule(
-            "virtual:react-router/server-build"
-          ) as Promise<ServerBuild>,
-      })
-    );
-  }
 }
